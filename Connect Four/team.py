8d7ec49b551797f2bab2009af6a95760bfc9cc28
Zoniklalessimo <zoniklalessimo@gmail.com>
2017-03-19 21:54:26 +0100

    First Scripts

---
+++ Connect Four/team.py
@@ -0,0 +1,15 @@
+class Team:
+	def __init__(self, color, team_size):
+		self.color = color
+		self.team_size = team_size
+		self.chips = list(list())
+
+	def add_chip(x, y):
+		chips.append([x, y])
+		if test_win():
+			return True
+		else:
+			return False
+	
+	def test_win():
+		
